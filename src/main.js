3936561dca951ea55ee7cdc70661e9a7dd0f3b18
cynthiagomezmontoya <cgomezm066@gmail.com>
2019-07-08 00:13:12 -0400

    actualizacion

--- src/main.js
+++ src/main.js
@@ -3,27 +3,6 @@
 const myData = POKEMON.pokemon; //const para acceder a la data
 const container = document.getElementById("root");
 
- for (let i = 0; i< myData.length; i++){
-  
-  let pokemonInfo = document.createElement("div");
- pokemonInfo.id = "pokeInfo";
- pokemonInfo.className = "pokeInfo";
-
- let pokemonName = document.createElement("p");
- pokemonName.textContent = myData[i].name;
-
- let pokemonImagen = document.createElement("img");
- pokemonImagen.src = myData[i].img;
- pokemonImagen.className = "pokeImagen";
-
- let pokemonId = document.createElement("p");
- pokemonId.textContent = myData[i].id;
-
- pokemonInfo.appendChild(pokemonName);
- pokemonInfo.appendChild(pokemonImagen);
- pokemonInfo.appendChild(pokemonId);
-document.getElementById("container").appendChild(pokemonInfo).innerHTML;
-
 //DOM FUNCIÓN FILTRAR
  document.getElementById("pok-filter").addEventListener("change",() => {
   let tipoFilter = document.getElementById("pok-filter").value; 
@@ -31,26 +10,44 @@ document.getElementById("container").appendChild(pokemonInfo).innerHTML;
   container.innerHTML ="";
   lastResult.forEach(element => {
       container.innerHTML += `
-      <div class="img">
-      <img src="${element.img}">
-      </div>
-     <h3>${element.type}</h3>
-      <p>${element.name}</p>`
+      <div class="flip-card">
+<div class="flip-card-inner">
+
+  <div class="flip-card-front">
+    <img id="img-card" src="${element.img}" alt="Avatar">
+   </div>
+
+   <div class="flip-card-back">
+     <p>${element.name}</p>
+     <p id="type-poke">Tipo:${element.type}</p>
+     <p"number-poke">${element.num}</p>
+   </div>
+ </div>
+</div>`
     })
   })
 //ordenar
+
 const pokeOrder = document.getElementById("name");
 pokeOrder.addEventListener('change', () => {
     let sortOrder = pokeOrder.value;
     let selectionOrder = sortPokes(myData,"name",sortOrder);
     container.innerHTML ="";
     selectionOrder.forEach(element=> {
-        container.innerHTML += `
-        <div class="img">
-     <img src="${element.img}">
-     </div>
-     <h3>${element.name}</h3>
-     <p>${element.type}</p>`
+      container.innerHTML += `
+      <div class="flip-card">
+<div class="flip-card-inner">
+
+  <div class="flip-card-front">
+    <img id="img-card" src="${element.img}" alt="Avatar">
+   </div>
+
+   <div class="flip-card-back">
+     <p>${element.name}</p>
+     <p id="type-poke">Tipo:${element.type}</p>
+     <p"number-poke">${element.num}</p>
+   </div>
+ </div>
+</div>`
     })
   })
-  
--- test/data.spec.js
+++ test/data.spec.js
@@ -4,13 +4,37 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe('filter', () => {
+const data = [
+  {
+    "id": 1,
+    "num": "001",
+    "name": "Bulbasaur",
+    "type": [
+      "Grass",
+      "Poison"
+    ]
+  }, {
+    "id": 46,
+    "num": "046",
+    "name": "Paras",
+    "type": [
+      "Bug",
+      "Grass"
+    ]
+  }, {
+    "id": 101,
+    "num": "101",
+    "name": "Electrode",
+    "type": [
+      "Electric",
+    
+    ]
+  }
+]
+
+describe('data.filterType', () => {
   
   it('debería ser una función', () => {
-    assert.equal(typeof filter, 'function');
-  });
-
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+    assert.equal(typeof window.data.filterType, 'function');
   });
 })
